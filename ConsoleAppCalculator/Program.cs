89b4976a307827e08b0ec96d3e5c7cf4a61e5952
Sven.daniel.carlsson@gmail.com <Sven.daniel.carlsson@gmail.com>
2021-09-03 14:06:20 +0200

    Added experimental method Exponentiation

--- ConsoleAppCalculator/Program.cs
+++ ConsoleAppCalculator/Program.cs
@@ -13,33 +13,35 @@ namespace ConsoleAppCalculator
                                   "\n1. Addition" +
                                   "\n2. Subtraction" +
                                   "\n3. Division" +
-                                  "\n4. Multiplication");
+                                  "\n4. Multiplication" +
+                                  "\n5. Exponentiation (experimental)");
 
                 double selection = AskUserForNumber("what you want to do: ");
+                Console.Clear();
                 switch (selection)
                 {
                     case 1:
-                        Console.Clear();
                         Addition();
                         break;
 
                     case 2:
-                        Console.Clear();
                         Subtraction();
                         break;
 
                     case 3:
-                        Console.Clear();
                         Division();
                         break;
 
                     case 4:
-                        Console.Clear();
                         Multiplication();
                         break;
 
+                    case 5:
+                        Exponentiation();
+                        break;
+
                     default:
-                        Console.WriteLine("\nIncorrect selection");
+                        Console.WriteLine("Incorrect selection");
                         break;
                 }
 
@@ -116,10 +118,25 @@ namespace ConsoleAppCalculator
         static void Division()
         {
             Console.WriteLine("~ Division ~");
-
             double num1, num2, numSum;
-            num1 = AskUserForNumber("a number to be divided: ");
-            num2 = AskUserForNumber("a number to divide with: ");
+
+            do
+            {
+                num1 = AskUserForNumber("a number to be divided: ");
+                if(num1 == 0)
+                {
+                    Console.WriteLine(num1 + " cant be divided");
+                }
+            } while (num1 == 0);
+
+            do
+            {
+                num2 = AskUserForNumber("a number to divide with: ");
+                if(num2 == 0)
+                {
+                    Console.WriteLine(num1 + " can not be divided by " + num2);
+                }
+            } while (num2 == 0);
 
             numSum = num1 / num2;
             Console.WriteLine("\n" + num1 + " / " + num2 + " = " + numSum + "\n");
@@ -132,10 +149,37 @@ namespace ConsoleAppCalculator
 
             double num1, num2, numSum;
             num1 = AskUserForNumber("your first number: ");
-            num2 = AskUserForNumber("a number to multiply by: ");
+            num2 = AskUserForNumber("a number to multiply with: ");
 
             numSum = num1 * num2;
             Console.WriteLine("\n" + num1 + " * " + num2 + " = " + numSum + "\n");
+        }
+
+
+        static void Exponentiation()
+        {
+            //calculations need improvement
+            Console.WriteLine("~ Exponentiation ~");
+            
+            double num1, num2, numSum;
+            num1 = AskUserForNumber("your base number: ");
+            num2 = AskUserForNumber("your exponent number: ");
+
+            /*
+            double _num1 = Math.Abs(num1);
+            double _num2 = Math.Abs(num2);
+
+            numSum = Math.Pow(_num1, _num2);
+            double _numSum = -(1 / numSum);
+            */
+            
+            numSum = Math.Pow(num1, num2);
+            if(num1 < 0 && num2 % 2 == 0)
+            {
+                numSum = numSum * -1;
+            }
+            
+            Console.WriteLine("\n" + num1 + " ^ " + num2 + " = " + numSum + "\n");
         }
     }
 }
